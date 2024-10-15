bfaff557365fb333cda1c3362ce51691349642b6
Davide Pezzotti <103988251+dvdpezzo@users.noreply.github.com>
2024-10-15 13:32:25 +0200

    update 19 files and delete 4 files

--- src/main/java/it/ingbs/ingegneria_software/gestioneAccesso/MenuAccesso.java
+++ src/main/java/it/ingbs/ingegneria_software/gestioneAccesso/MenuAccesso.java
@@ -1,32 +1,18 @@
 package it.ingbs.ingegneria_software.gestioneAccesso;
 
-import it.ingbs.ingegneria_software.utilitaGenerale.*;
-
-/* MENU DI ACCESSO MOLTO BASILARE, BISOGNA CREARE UNA CLASSE DI GESTIONE DEL FILE DI ACCESO (UNA MAPPA) */
+import it.ingbs.ingegneria_software.model.Utente;
+import it.ingbs.ingegneria_software.utilitaGenerale.InputDati;
 
 public class MenuAccesso {
 
-    private static final String [] AZIONI_POSSIBILI ={"ACCESSO COORDINATORE", "ACCESSO FRUITORE"};
-
-    public void avvio(GestoreAccesso gestoreAccesso) {
-        int scelta;
-        MenuUtil menuLogin = new MenuUtil("MENU DI ACCESSO:", AZIONI_POSSIBILI);
+    public static Utente avvioLogin(){
+        GestoreAccesso gestoreAccesso = new GestoreAccesso();
+        Utente result;
         do {
-            scelta = menuLogin.scegli();
-            switch(scelta){
-                //accesso coordinatore
-                case 1:
-                    String nomeConfig= InputDati.leggiStringaNonVuota("Inserire nome utente: ");
-                    String passConfig= InputDati.leggiStringaNonVuota("Inserisci password: ");
-                    gestoreAccesso.accessoUtente(nomeConfig, passConfig);
-                    break;
-                //accesso fruitore
-                case 2:
-                    String nomeFruit= InputDati.leggiStringaNonVuota("Inserire nome utente: ");
-                    String passFruit= InputDati.leggiStringaNonVuota("Inserisci password: ");
-                    gestoreAccesso.accessoUtente(nomeFruit, passFruit);
-                    break;
-            }
-        }while(scelta!=0);
+            String nomeUtente= InputDati.leggiStringaNonVuota("Inserire nome utente: ");
+            String passUtente= InputDati.leggiStringaNonVuota("Inserisci password: ");
+            result = gestoreAccesso.accessoUtente(nomeUtente, passUtente);
+        } while (result == null);
+        return result;
     }
 }
