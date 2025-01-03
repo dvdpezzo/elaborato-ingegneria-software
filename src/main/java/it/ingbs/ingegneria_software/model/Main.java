1b1cf6e7b4145a9b43e9ea04b67cc3bc606e1277
djmezv2 <f.mazza002@studenti.unibs.it>
2025-01-03 09:34:46 +0100

    Gestore Fruitore

--- src/main/java/it/ingbs/ingegneria_software/model/Main.java
+++ src/main/java/it/ingbs/ingegneria_software/model/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.logging.*;
 import it.ingbs.ingegneria_software.controller.GestoreMenu;
 import it.ingbs.ingegneria_software.gestione_accesso.MenuAccesso;
+import it.ingbs.ingegneria_software.utilita_generale.MenuUtil;
 
 public class Main {
 	//setup logger: non toccare
@@ -24,26 +25,45 @@ public class Main {
         }
     }
 	
+//creazione del menu di accesso 
+    private final static String [] VOCI = {"ACCESSO CONFIGURATORE","ACCESSO FRUITORE"};
+    static MenuUtil menuAccesso = new MenuUtil("MENU DI ACCESSO UTENTE:",VOCI);
+    
+
 	public static void main(String[] args) throws IOException {
 
+        //oggetti di classi che gestiscono dati e informazioni
+        GestoreFruitore gf = new GestoreFruitore();
+	    GestoreComprensorio gc = new GestoreComprensorio();
 
+
+        
 		//creazione istanza del sistema generale:
 		GestoreMenu sistemaGenerale = new GestoreMenu();
 
-		//chiedi credenziali d'accesso:
-		Utente utente = MenuAccesso.loginConfiguratore();
+    
+        boolean accesso=true;
 
-		//da rivedere il modo in cui differenzia utente da configuratore, nella versione 2
+       do{
+           int opzione =menuAccesso.scegli();
+           switch(opzione){
+            
+            case 1:
+                Utente utente = MenuAccesso.loginConfiguratore();
+                  if (utente instanceof Configuratore){
+                    //se utente è di tipo Configuratore, richiama il menu back-end:
+                    sistemaGenerale.backEnd((Configuratore) utente, true);
+                  }
+                  accesso=false;
+                break;
 
-		if (utente instanceof Configuratore){
-			//se utente è di tipo Configuratore, richiama il menu back-end:
-            sistemaGenerale.backEnd((Configuratore) utente);
-		} else {
-			//altrimenti richiama il menu utente:
-			//sistemaGenerale.frontEnd();
-		}
-		
-
-	}
+            case 2:
+            MenuAccesso.menuFruitore();
+            accesso=false;
+            break;
+           }
+        }while(accesso);
+    }
 
 }
+
