977f20192cf0bddf0ad271fbb9e0cff5bfaada6b
djmezv2 <f.mazza002@studenti.unibs.it>
2025-04-01 14:02:44 +0200

    aggiunta di try catch nel metodo main

--- src/main/java/it/ingbs/ingegneria_software/model/Main.java
+++ src/main/java/it/ingbs/ingegneria_software/model/Main.java
@@ -2,6 +2,11 @@ package it.ingbs.ingegneria_software.model;
 
 import java.io.IOException;
 
+import it.ingbs.ingegneria_software.Eccezioni.CampoNotFoundException;
+import it.ingbs.ingegneria_software.Eccezioni.CategoriaNotFoundException;
+import it.ingbs.ingegneria_software.Eccezioni.CategoriaOmonimaException;
+import it.ingbs.ingegneria_software.Eccezioni.IllegalCampoException;
+import it.ingbs.ingegneria_software.Eccezioni.PadreNotFoundException;
 import it.ingbs.ingegneria_software.controller.GestoreMenu;
 import it.ingbs.ingegneria_software.gestione_accesso.GestoreAccesso;
 import it.ingbs.ingegneria_software.gestione_accesso.MenuAccesso;
@@ -56,12 +61,22 @@ public class Main {
             } while (opzione != 0);
             gestoreFile.creaSalvataggio();
 
-        } catch (IOException ex) {
+     } catch (IOException ex) {
             System.out.println("Errore nel caricamento dei dati");
-        } catch (Exception ex) {
+        } catch (CampoNotFoundException ex) {
             System.out.println("Errore generico");
+        } catch(CategoriaNotFoundException ex) {
+            System.out.println("Errore nella categoria");
+        } catch (CategoriaOmonimaException ex){
+            System.out.println("Errore nella categoria omonima: " + ex.getMessage());
+        } catch (IllegalCampoException ex) {
+            System.out.println("Errore nel campo: " + ex.getMessage());
+        } catch (PadreNotFoundException ex){
+            System.out.println("Errore nel padre: " + ex.getMessage());
         }
-
+        catch (Exception ex) {
+            System.out.println("Errore sconosciuto: " + ex.getMessage());
+        }
     }
 }
 
