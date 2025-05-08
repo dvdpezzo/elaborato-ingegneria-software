307bb7f079adc15f1f26ec8550efd4002514bb92
DESKTOP-M9BOQ6L\franc <loredjmez@gmail.com>
2025-05-08 16:24:02 +0200

    Creazione interfaccia UtilityHandler

---
+++ src/main/java/it/ingbs/ingegneria_software/utilita_generale/UtilityHandler.java
@@ -0,0 +1,14 @@
+package it.ingbs.ingegneria_software.utilita_generale;
+
+public interface UtilityHandler {
+    
+    void view();
+
+    void rimuovi();
+
+    void aggiungi();
+
+    void salva();
+
+
+}
--- src/main/java/it/ingbs/ingegneria_software/utilita_generale/Viewable.java
+++
@@ -1,7 +0,0 @@
-package it.ingbs.ingegneria_software.utilita_generale;
-
-public interface Viewable {
-    
-    void view(); // Metodo per stampare l'oggetto
-
-}
